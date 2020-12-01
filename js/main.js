342c897b4a42f0ef8831455de9fcbf882f896349
Bryan Larsson <bryanlarsson99@hotmail.com>
2020-12-01 15:35:28 +0100

    complete

--- js/main.js
+++ js/main.js
@@ -8,132 +8,111 @@ let peopleSelect = document.querySelector('#select-people');
 let sortBy = document.querySelector('#sort');
 let selectConMovie = document.querySelector('#movie');
 let img = document.querySelector('#movie-details .image');
+let nav = document.querySelector('#links');
+let peopleSection = document.querySelector('#people-section');
+let peopleDetails = document.querySelector('#people-details');
+let locationSection = document.querySelector('#location-section');
+let locationDetail = document.querySelector('#location-details');
 
+
+//All eventListners
 startBtn.addEventListener('click', showContent);
+document.getElementById('sgPeople').addEventListener('click', characters);
+document.getElementById('sgLocations').addEventListener('click', locations);
+sortBy.addEventListener('change', displayMovieSelect);
 
 function showContent() {
     toggleHidden.classList.toggle('hidden');
+    nav.classList.toggle('hidden');
     startDiv.className += 'hidden';
+    fetchData();
 }
 
-//This is the data variabel
-//Its a global variabel because i want to
-//use it in my functions
-let allMovieData; 
-
-let allPersonData;
+let allData; //This is my Data variabel, made it global for easy use.
 
 
-//Just trying out the DOMContentLoaded event
-document.addEventListener('DOMContentLoaded', async () => {
-    await fetchMovieData();
-    await movieOptionsTag(allMovieData);
-    movieSelect.onchange = () => {
-        for (let i = 0; i < allMovieData.length; i++) {
-            if (allMovieData[i].title === movieSelect.value) {
-                selectedMovieDetails(allMovieData[i]);
-                return;
+//Fetching all links from studio ghibli
+let movieData = fetch('https://ghibliapi.herokuapp.com/films')
+    .then(resp => resp.json());
+let peopleData = fetch('https://ghibliapi.herokuapp.com/people')
+    .then(resp => resp.json());
+let locationData = fetch('https://ghibliapi.herokuapp.com/locations')
+    .then(resp => resp.json());
+
+//Fetching the data from all links
+let fetchData = async function () {
+    try {
+        let data = await Promise.all([movieData, peopleData, locationData]);
+        allData = data;
+        displayMovieSelect();
+        movieOptionsTag(allData[0]);
+        movieSelect.onchange = () => {
+            for (let i = 0; i < allData[0].length; i++) {
+                if (allData[0][i].title === movieSelect.value) {
+                    selectedMovieDetails(allData[0][i]);
+                    return;
+                }
             }
         }
+        console.log(allData);
+        
     }
-})
+    catch (message) {
+        throw new Error(message);
+    }
+};
 
 
-//Fetching the data from studio ghibli
-let fetchMovieData = async () => {
-    await fetch('https://ghibliapi.herokuapp.com/films')
-        .then(response => response.json())
-        .then(data => {
-            allMovieData = data;
-            console.log(allMovieData);
-            let htmlContent = '';
-            for (let movies of allMovieData) {
-                htmlContent += `<a href="#"><section>`;
-
-                htmlContent += '<div class="movie-con">'
-                htmlContent += '<div class="img-container">';
-                htmlContent += `<img src="../img/${movies.id}.jpg">`;
-                htmlContent += '</div>';
-
-                htmlContent += '<div class="desc-container">';
-                htmlContent += `<h3>${movies.title}</h3>`;
-                htmlContent += `<b>Movie description</b>`;
-                htmlContent += `<p>${movies.description}</p>`;
-                htmlContent += '</div>';
-                htmlContent += '</div>';
-
-                htmlContent += `</section></a>`;
-
-                content.innerHTML = htmlContent;
-            }
-        });
-
-}
-
-let fetchPersonData = async () => {
-    await fetch('https://ghibliapi.herokuapp.com/people')
-    .then(response => response.json())
-    .then(data => {
-        allPersonData = data;
-        console.log(allPersonData);
-        let testContent = '';
-        for(let testPeople of allPersonData){
-            testContent += testPeople.name;
-
-            content.innerHTML = testContent;
-        }
-    });
-}
 
 //Creating the sorting system by checking the value of select
-let displayMovieSelect = () => {
+function displayMovieSelect() {
     let selectValue = sortBy.value;
     let htmlContent = '';
     switch (selectValue) {
         case 'all':
             //Sorting the data by release year from old - New
-            let sortByRelease = allMovieData.sort((a, b) => a.release_date - b.release_date);
+            let sortByRelease = allData[0].sort((a, b) => a.release_date - b.release_date);
             for (let movies of sortByRelease) {
-                htmlContent += `<a href="#"><section>`;
+                htmlContent += `<section>`;
 
-                htmlContent += '<div class="movie-con">'
-                htmlContent += '<div class="img-container">';
-                htmlContent += `<img src="../img/${movies.id}.jpg">`;
+                htmlContent += '<div class="container">'
+                
+                htmlContent += `<img src="../img/${movies.id}.jpg" id="image">`;
+                
+
+                htmlContent += '<div class="card">';
+                htmlContent += `<h1>${movies.title}</h3>`;
+                htmlContent += `<p>${movies.description.substring(0, 200)}...</p>`;
+                htmlContent += '</div>';
                 htmlContent += '</div>';
 
-                htmlContent += '<div class="desc-container">';
-                htmlContent += `<h3>${movies.title}</h3>`;
-                htmlContent += `<b>Movie description</b>`;
-                htmlContent += `<p>${movies.description}</p>`;
-                htmlContent += '</div>';
-                htmlContent += '</div>';
-
-                htmlContent += `</section></a>`;
+                htmlContent += `</section>`;
 
                 content.innerHTML = htmlContent;
+
             }
             selectConMovie.className = 'hidden';
             break;
 
         case 'release':
             //Sorting the data by release year from new - old
-            let sort = allMovieData.sort((a, b) => b.release_date - a.release_date);
+            let sort = allData[0].sort((a, b) => b.release_date - a.release_date);
             for (let movies of sort) {
-                htmlContent += `<a href="#"><section>`;
+                htmlContent += `<section>`;
 
-                htmlContent += '<div class="movie-con">'
+                htmlContent += '<div class="container">'
                 htmlContent += '<div class="img-container">';
-                htmlContent += `<img src="../img/${movies.id}.jpg">`;
+                htmlContent += `<img src="../img/${movies.id}.jpg" id="image">`;
                 htmlContent += '</div>';
 
-                htmlContent += '<div class="desc-container">';
-                htmlContent += `<h3>${movies.title}</h3>`;
-                htmlContent += `<i>Release Year : ${movies.release_date}</i>`;
-                htmlContent += `<p>${movies.description}</p>`;
+                htmlContent += '<div class="card">';
+                htmlContent += `<h1>${movies.title}</h3>`;
+                htmlContent += `<p>${movies.description.substring(0, 200)}...</p>`;
+                htmlContent += `<p><b>Released: </b><i>${movies.release_date}</i></p>`;
                 htmlContent += '</div>';
                 htmlContent += '</div>';
 
-                htmlContent += `</section></a>`;
+                htmlContent += `</section>`;
 
                 content.innerHTML = htmlContent;
             }
@@ -142,23 +121,23 @@ let displayMovieSelect = () => {
 
         case 'director':
             //Sorting by director and placing them in order
-            let test = allMovieData.sort((a, b) => a.director.localeCompare(b.director, 'or', { sensitivity: 'base' }));
+            let test = allData[0].sort((a, b) => a.director.localeCompare(b.director, 'or', { sensitivity: 'base' }));
             for (let movies of test) {
-                htmlContent += `<a href="#"><section>`;
+                htmlContent += `<section>`;
 
-                htmlContent += '<div class="movie-con">'
+                htmlContent += '<div class="container">'
                 htmlContent += '<div class="img-container">';
-                htmlContent += `<img src="../img/${movies.id}.jpg">`;
+                htmlContent += `<img src="../img/${movies.id}.jpg" id="image">`;
                 htmlContent += '</div>';
 
-                htmlContent += '<div class="desc-container">';
-                htmlContent += `<h3>${movies.title}</h3>`;
-                htmlContent += `<i>Director : ${movies.director}</i>`;
-                htmlContent += `<p>${movies.description}</p>`;
+                htmlContent += '<div class="card">';
+                htmlContent += `<h1>${movies.title}</h3>`;
+                htmlContent += `<p>${movies.description.substring(0, 200)}...</p>`;
+                htmlContent += `<p><b>Director: </b><i>${movies.director}</i></p>`;
                 htmlContent += '</div>';
                 htmlContent += '</div>';
 
-                htmlContent += `</section></a>`;
+                htmlContent += `</section>`;
 
                 content.innerHTML = htmlContent;
             }
@@ -180,29 +159,108 @@ let movieOptionsTag = (movieObj) => {
         optionTag.innerText = movieObj[i].title;
         movieSelect.appendChild(optionTag);
     }
-}
+};
 
 //Displays this when a movie is selected
 let selectedMovieDetails = (selectedMovie) => {
-    fetchPersonData();
     content.innerHTML = '';
 
-    let title = document.createElement('h3');
+    let info = document.createElement('div');
+    info.id = 'info';
+    let card = document.createElement('div');
+    card.className = 'card';
+    let title = document.createElement('h1');
     let year = document.createElement('p');
     let desc = document.createElement('p');
-    let characters = document.createElement('p');
+    let director = document.createElement('p');
+    let producer = document.createElement('p');
     let div = document.createElement('div');
 
-    div.innerHTML = `<img src="../img/${selectedMovie.id}.jpg">`;
+    div.innerHTML = `<img src="../img/${selectedMovie.id}.jpg" id="selected-movie-image">`;
     title.innerText = selectedMovie.title;
-    year.innerText = 'Released:' + selectedMovie.release_date;
-    desc.innerText = selectedMovie.description;
-    console.log(selectedMovie.people);
+    year.innerHTML = '<b>'+'Released: '+'</b>' + '<i>' + selectedMovie.release_date + '</i>';
+    desc.innerHTML = '<b>' + 'Description' + '</b>' + '<br>' + '<br>' + selectedMovie.description;
+    director.innerHTML = '<b>'+'Director: '+'</b>' + '<i>' + selectedMovie.director + '</i>';
+    producer.innerHTML = '<b>'+'Producer: '+'</b>' + '<i>' + selectedMovie.producer + '</i>';
 
-    content.appendChild(div);
-    content.appendChild(title);
-    content.appendChild(year);
-    content.appendChild(desc);
-    content.appendChild(characters);
+    content.appendChild(info);
+    info.appendChild(div);
+    info.appendChild(card);
+    card.appendChild(title);
+    card.appendChild(desc);
+    card.appendChild(year);
+    card.appendChild(director);
+    card.appendChild(producer);
+};
+
+function characters() {
+    toggleHidden.innerHTML = '';
+    locationSection.className = 'hidden';
+    peopleSection.className = '';
+
+    let peopleContainer = document.createElement('div');
+    peopleContainer.setAttribute('class', 'people-container');
+    peopleDetails.appendChild(peopleContainer);
+
+    for(let character of allData[1]){
+        console.log(character);
+        let card = document.createElement('div');
+        card.setAttribute('class', 'normal-card');
+        
+        let person = document.createElement('h1');
+        person.innerText = character.name;
+
+        let gender = document.createElement('p');
+        gender.innerText = `Gender: ${character.gender}`;
+
+        let age = document.createElement('p');
+        age.innerText = `Age: ${character.age}`;
+
+        let eyeColor = document.createElement('p');
+        eyeColor.innerText = `Eye color: ${character.eye_color}`;
+
+        let hairColor = document.createElement('p');
+        hairColor.innerText = `Hair color: ${character.hair_color}`;
+
+        peopleContainer.appendChild(card);
+        card.appendChild(person);
+        card.appendChild(gender);
+        card.appendChild(age);
+        card.appendChild(eyeColor);
+        card.appendChild(hairColor);
+    }
 }
-sortBy.addEventListener('change', displayMovieSelect);
+
+function locations() {
+    toggleHidden.innerHTML = '';
+    locationSection.className = '';
+    peopleSection.className = 'hidden';
+
+    let peopleContainer = document.createElement('div');
+    peopleContainer.setAttribute('class', 'people-container');
+    locationDetail.appendChild(peopleContainer);
+
+    for(let location of allData[2]){
+        let card = document.createElement('div');
+        card.setAttribute('class', 'normal-card');
+        
+        let locations = document.createElement('h1');
+        locations.innerText = location.name;
+
+        let climate = document.createElement('p');
+        climate.innerText = `Climate: ${location.climate}`;
+
+        let terrain = document.createElement('p');
+        terrain.innerText = `Terrain: ${location.terrain}`;
+
+        let water = document.createElement('p');
+        water.innerText = `Surface water: ${location.surface_water}`;
+
+
+        peopleContainer.appendChild(card);
+        card.appendChild(locations);
+        card.appendChild(climate);
+        card.appendChild(terrain);
+        card.appendChild(water);
+    }
+}
